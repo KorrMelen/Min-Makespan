e6fc64d6eb9145535e2e50efdeba5b12507e899d
Marc <marc.albert.lebras@gmail.com>
2020-01-13 12:48:37 +0100

    durée + affichage

--- Min_Makespan.py
+++ Min_Makespan.py
@@ -3,6 +3,7 @@
 from statistics import mean
 from math import ceil
 from sys import exit
+from time import time
 import Min_Makespan_Instance
 import Min_Makespan_Algos
 
@@ -38,34 +39,60 @@ while(True):
 	if type(D[0]) == int :
 		borneInfMax = max(D)
 		borneInfMoy = ceil(sum(D)/len(M))
+		timeLSA = time()
 		resLSA = max(Min_Makespan_Algos.LSA(D.copy(),M.copy()))
+		timeLSA = time() - timeLSA
 		ratioLSA = resLSA / max(borneInfMoy,borneInfMax)
+		timeLPT = time()
 		resLPT = max(Min_Makespan_Algos.LPT(D.copy(),M.copy()))
+		timeLPT = time() - timeLPT
 		ratioLPT = resLPT / max(borneInfMax,borneInfMoy)
+		timeMYALGO = time()
 		resMYALGO = max(Min_Makespan_Algos.MYALGO(D.copy(),M.copy()))
+		timeMYALGO = time() - timeMYALGO
 		ratioMYALGO = resMYALGO / max(borneInfMax,borneInfMoy)
 
 		print("Borne Inférieur Maximun = ",borneInfMax)
 		print("Borne Inférieur Moyenne = ",borneInfMoy)
+		print("----------------------------------------")
 		print("Résultat LSA = ",resLSA)
 		print("Ratio LSA = ",ratioLSA)
+		print("Durée de LSA = ",timeLSA)
+		print("----------------------------------------")
 		print("Résultat LPT = ",resLPT)
 		print("Ratio LPT = ",ratioLPT)
+		print("Durée de LPT = ",timeLPT)
+		print("----------------------------------------")
 		print("Résultat MYALGO = ",resMYALGO)
 		print("Ratio MYALGO = ",ratioMYALGO)
+		print("Durée de MYALGO = ",timeMYALGO)
 
 	else:
 		ratioLSA = 0
 		ratioLPT = 0
 		ratioMYALGO = 0
+		timeLSA = 0
+		timeLPT = 0
+		timeMYALGO = 0
 		for i in range(len(D)):
 			borneInfMax = max(D[i])
 			borneInfMoy = ceil(sum(D[i])/len(M[i]))
+			timeDebut = time()
 			ratioLSA += max(Min_Makespan_Algos.LSA(D[i].copy(),M[i].copy())) / max(borneInfMoy,borneInfMax)
+			timeLSA += time() - timeDebut
+			timeDebut = time()
 			ratioLPT += max(Min_Makespan_Algos.LPT(D[i].copy(),M[i].copy())) / max(borneInfMax,borneInfMoy)
+			timeLPT += time() - timeDebut
+			timeDebut = time()
 			ratioMYALGO += max(Min_Makespan_Algos.MYALGO(D[i].copy(),M[i].copy())) / max(borneInfMax,borneInfMoy)
+			timeMYALGO += time() - timeDebut
 		print("Ratio moyen LSA = ",(ratioLSA/len(D)))
+		print("Durée Moyenne LSA = ",(timeLSA/len(D)))
+		print("----------------------------------------")
 		print("Ratio moyen LPT = ",(ratioLPT/len(D)))
+		print("Durée Moyenne LPT = ",(timeLPT/len(D)))
+		print("----------------------------------------")
 		print("Ratio moyen MYALGO = ",(ratioMYALGO/len(D)))
+		print("Durée Moyenne MYALGO = ",(timeMYALGO/len(D)))
 
 	print("#################################################################")
